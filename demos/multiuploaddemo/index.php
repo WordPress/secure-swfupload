9f5aa7b6537b7c2258b453336ca209afdd83f781
Jake Roberts <jakeroberts@gmail.com>
2008-01-22 01:33:11 +0000

    Moved the doctype after session_start

--- demos/multiuploaddemo/index.php
+++ demos/multiuploaddemo/index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 	session_start();
 
@@ -8,6 +7,7 @@
 	}
 
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head>
     <title>SWFUpload v2.0 Multi-Instance Demo</title>
